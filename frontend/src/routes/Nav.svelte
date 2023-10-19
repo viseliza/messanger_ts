2a71475d47a773921220f8e760ff1962a9377054
viseliza 1 <nenyxen123@gmail.com>
2023-10-19 13:13:18 +0000

    that not worked

---
+++ frontend/src/routes/Nav.svelte
@@ -0,0 +1,323 @@
+<script>
+  import { page } from "$app/stores";
+  import user from "$lib/images/user.svg";
+  import home from "$lib/images/home.svg";
+  import message from "$lib/images/message.svg";
+  import { swapColor } from "$lib/js/swapColor.js";
+</script>
+
+<nav class="sidebar">
+  <header>
+    <div class="image-text">
+      <span class="image" />
+
+      <div class="text logo-text">
+        <span class="name" />
+        <span class="profession" />
+      </div>
+    </div>
+  </header>
+  <!-- ./header -->
+
+  <div class="menu-bar">
+    <div class="menu">
+      <ul class="menu-links">
+        <li class="nav-link">
+          <a href="#">
+            <img src={home} />
+            <span class="tooltip-text">Home</span>
+          </a>
+        </li>
+        <!-- ./nav-link -->
+        <li class="nav-link">
+          <a href="#">
+            <img src={message} />
+            <span class="tooltip-text">Messanger</span>
+          </a>
+        </li>
+      </ul>
+      <!-- ./menu-links -->
+    </div>
+    <!-- ./menu -->
+
+    <div class="bottom-content">
+      <li class="">
+        <a href="#">
+          <img src={user} />
+          <span class="tooltip-text">Log out</span>
+        </a>
+      </li>
+
+      <li class="mode">
+        <div class="sun-moon">
+          <i class="bx bx-moon icon moon" />
+          <i class="bx bx-sun icon sun" />
+        </div>
+
+        <div class="toggle-switch">
+          <span class="switch" />
+        </div>
+      </li>
+      <!-- ./mode -->
+    </div>
+    <!-- ./botton-content -->
+  </div>
+  <!-- ./menu-bar -->
+</nav>
+
+
+<style>
+  /* ===== Sidebar ===== */
+  .sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 88px;
+    padding: 10px 14px;
+    background: var(--sidebar-color);
+    transition: var(--tran-05);
+    z-index: 100;
+  }
+
+  /* ===== Reusable code - Here ===== */
+  .sidebar li {
+    height: 50px;
+    list-style: none;
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+  }
+
+  .sidebar header .image,
+  .sidebar .icon {
+    min-width: 60px;
+    border-radius: 6px;
+  }
+
+  .sidebar .icon {
+    min-width: 60px;
+    border-radius: 6px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+  }
+
+  .sidebar .text,
+  .sidebar .icon {
+    color: var(--text-color);
+    transition: var(--tran-03);
+  }
+
+  .sidebar .text {
+    font-size: 17px;
+    font-weight: 500;
+    white-space: nowrap;
+    opacity: 1;
+  }
+  .sidebar.close .text {
+    opacity: 0;
+  }
+  /* =========================== */
+
+  .sidebar header {
+    position: relative;
+  }
+
+  .sidebar header .image-text {
+    display: flex;
+    align-items: center;
+  }
+  .sidebar header .logo-text {
+    display: flex;
+    flex-direction: column;
+  }
+  header .image-text .name {
+    margin-top: 2px;
+    font-size: 18px;
+    font-weight: 600;
+  }
+
+  header .image-text .profession {
+    font-size: 16px;
+    margin-top: -2px;
+    display: block;
+  }
+
+  .sidebar header .image {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .sidebar header .image img {
+    width: 40px;
+    border-radius: 50%;
+  }
+
+  .sidebar header .toggle {
+    position: absolute;
+    top: 50%;
+    right: -25px;
+    transform: translateY(-50%) rotate(180deg);
+    height: 25px;
+    width: 25px;
+    background-color: var(--primary-color);
+    color: var(--sidebar-color);
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 22px;
+    cursor: pointer;
+    transition: var(--tran-05);
+  }
+
+
+  .sidebar li a {
+    list-style: none;
+    height: 100%;
+    background-color: transparent;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    border-radius: 6px;
+    text-decoration: none;
+    transition: var(--tran-03);
+  }
+
+  .sidebar li a:hover {
+    background-color: var(--primary-color);
+  }
+  .sidebar li a:hover .icon,
+  .sidebar li a:hover .text {
+    color: var(--sidebar-color);
+  }
+
+  .sidebar li a img {
+    margin: auto;
+  }
+
+  body.dark .sidebar li a:hover .icon,
+  body.dark .sidebar li a:hover .text {
+    color: var(--text-color);
+  }
+
+  .tooltip-text {
+    visibility: hidden;
+    position: absolute;
+    z-index: 1;
+    width: 100px;
+    color: white;
+    font-size: 12px;
+    margin-left: 70px;
+    background-color: var(--tooltip);
+    border-radius: 10px;
+    padding: 10px 15px 10px 15px;
+  }
+
+  li a:hover .tooltip-text {
+    visibility: visible;
+  }
+
+  .sidebar .menu-bar {
+    height: calc(100% - 55px);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    overflow-y: scroll;
+  }
+  .menu-bar::-webkit-scrollbar {
+    display: none;
+  }
+  .sidebar .menu-bar .mode {
+    border-radius: 6px;
+    background-color: var(--primary-color-light);
+    position: relative;
+    transition: var(--tran-05);
+  }
+
+  .menu-bar .mode .sun-moon {
+    height: 50px;
+    width: 60px;
+  }
+
+  .mode .sun-moon i {
+    position: absolute;
+  }
+  .mode .sun-moon i.sun {
+    opacity: 0;
+  }
+  body.dark .mode .sun-moon i.sun {
+    opacity: 1;
+  }
+  body.dark .mode .sun-moon i.moon {
+    opacity: 0;
+  }
+
+  .menu-bar .bottom-content .toggle-switch {
+    position: absolute;
+    right: 0;
+    height: 100%;
+    min-width: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 6px;
+    cursor: pointer;
+  }
+  .toggle-switch .switch {
+    position: relative;
+    height: 22px;
+    width: 40px;
+    border-radius: 25px;
+    background-color: var(--toggle-color);
+    transition: var(--tran-05);
+  }
+
+  .switch::before {
+    content: "";
+    position: absolute;
+    height: 15px;
+    width: 15px;
+    border-radius: 50%;
+    top: 50%;
+    left: 5px;
+    transform: translateY(-50%);
+    background-color: var(--sidebar-color);
+    transition: var(--tran-04);
+  }
+
+  body.dark .switch::before {
+    left: 20px;
+  }
+
+  .home {
+    position: relative;
+    top: 0;
+    top: 0;
+    left: 250px;
+    height: 100vh;
+    width: calc(100% - 250px);
+    background-color: var(--body-color);
+    transition: var(--tran-05);
+  }
+  .home .text {
+    font-size: 30px;
+    font-weight: 500;
+    color: var(--text-color);
+    padding: 12px 60px;
+  }
+
+  .sidebar.close ~ .home {
+    left: 78px;
+    height: 100vh;
+    width: calc(100% - 78px);
+  }
+  body.dark .home .text {
+    color: var(--text-color);
+  }
+</style>
